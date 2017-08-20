d73805d4f4d5c04b0639e20d2be7aefb1ef64f07
Cynicalbird <alex.ng.personal@gmail.com>
2017-08-20 10:55:14 -0400

    initial commit / version

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      enable-resize-watcher
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+        >
+          <v-list-tile-action>
+            <v-icon light v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in" appear>
+          <v-layout column align-center>
+            <router-view></router-view>
+          </v-layout>
+        </v-slide-y-transition>
+      </v-container>
+    </main>
+    <v-footer :fixed="fixed">
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'home', title: 'Home', to: '/' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'WICS - Ryerson'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container fluid>
+    <v-card>
+      <v-container>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-card-media
+              src="/static/v.png"
+              height="150px"
+              contain
+            ></v-card-media>
+          </v-flex>
+          <v-flex xs12>
+            <v-card-title>
+              <h2>Home</h2>
+              <blockquote>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque tortor turpis, sollicitudin at tristique vel,
+                rhoncus at libero. Nam vehicula sem vel placerat egestas.
+                In hac habitasse platea dictumst. Pellentesque tincidunt
+                dui eget tellus vulputate scelerisque. Suspendisse malesuada
+                consequat urna at laoreet. Vestibulum et risus semper, aliquet
+                diam eget, efficitur turpis. Duis lacus turpis, aliquet scelerisque
+                mauris sit amet, tristique efficitur enim. In hac habitasse platea
+                dictumst. Praesent tristique libero in mi pretium, non imperdiet leo
+                aliquet. In eget felis tellus. Mauris sagittis augue non mauris
+                ullamcorper,
+              </blockquote>
+            </v-card-title>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
