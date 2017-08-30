81562bfad84382929f192d88aa7f89808606440c
Cynicalbird <alex.ng.personal@gmail.com>
2017-08-30 13:51:55 -0400

    added back app script components and app theme back to light

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app dark>
+  <v-app light>
     <wics-nav-drawer></wics-nav-drawer>
     <main>
       <router-view></router-view>
@@ -7,6 +7,19 @@
     <wics-footer></wics-footer>
   </v-app>
 </template>
+
+<script>
+  import NavDrawer from './components/navigation/drawer/drawer.vue'
+  import Toolbar from './components/navigation/toolbar/Toolbar.vue'
+  import Footer from './components/navigation/footer/Footer.vue'
+  export default {
+    components: {
+      'wics-nav-drawer': NavDrawer,
+      'wics-toolbar': Toolbar,
+      'wics-footer': Footer
+    }
+  }
+</script>
 
 <style lang="stylus">
   @import './stylus/main'
