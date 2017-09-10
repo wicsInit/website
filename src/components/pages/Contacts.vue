3bbefa092e55a9b3f10e3bcd26eec572442a90f3
rlabana <labana_rk@hotmail.com>
2017-09-10 17:55:36 -0400

    slc pic as bg for contacts.vue page

--- src/components/pages/Contacts.vue
+++ src/components/pages/Contacts.vue
@@ -1,39 +1,30 @@
 <template>
   <div class="text-xs-center">
 
-    <!-- header img -->
-    <div class="crop">
-      <img src="/static/images/slc-large-banner.jpg"/>
-      <div class="title">
-        <h1>Contact</h1>
-      </div>
+    <div
+      class="card__media__background"
+      :style="backgroundStyle"
+    >
     </div>
 
-
-    <!-- header Text -->
-
+    <div>
+      <h1> Contact</h1>
+    </div>
     <v-layout justify-space-around>
       <span class="group pa-2">
         <a href="https://www.facebook.com/ryersonwics?fref=ts"><v-icon x-large>fa-facebook</v-icon></a>
         <v-icon x-large>fa-twitter</v-icon>
         <v-icon x-large>fa-instagram</v-icon>
         <v-icon x-large>fa-envelope</v-icon>
-        <v-icon x-large>fa-linkedin</v-icon>
+        <a href="https://ca.linkedin.com/company/ryersonwics"><v-icon x-large>fa-linkedin</v-icon></a>
       </span>
-    </v-layout>
+      </v-layout>
 
-  </div>
+    </div>
 </template>
 
 <style>
-  img{
-    width:100%;
-    height: 100%;
-  }
-  .crop{
-    width: 100%;
-    height: 200px;
-  }
+
   .title{
     position: absolute;
     top: 5%;
@@ -41,50 +32,63 @@
   }
 
   .fa-envelope:before{
-    font-size: 500%;
+    font-size: 400%;
     color: black;
-    position: absolute;
-    left: 70%;
-    top: 40%;
-    right:30%;
-    bottom: 60%;
-
+    margin: 2%;
   }
   .fa-facebook:before{
-    font-size: 700%;
+    font-size: 400%;
     color: darkblue;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    right:50%;
-    bottom: 50%;
+    margin: 2%;
   }
   .fa-twitter:before{
     font-size: 400%;
     color: deepskyblue;
-    position: absolute;
-    left: 40%;
-    top: 45%;
-    right:60%;
-    bottom: 60%;
+    margin: 2%;
   }
   .fa-linkedin:before{
-    font-size: 300%;
+    font-size: 400%;
     color: royalblue;
-    position: absolute;
-    left: 65%;
-    top: 67%;
-    right:25%;
-    bottom: 37%;
+    margin: 2%;
   }
   .fa-instagram:before{
-    font-size: 600%;
+    font-size: 400%;
     color: rebeccapurple;
-    position: absolute;
-    left: 30%;
-    top: 67%;
-    right:70%;
-    bottom: 33%;
+    margin: 2%;
   }
 </style>
 
+<script>
+  import Card from '../general/cards/Card.vue'
+  import Cards from '../general/cards/Cards.vue'
+  import Carousel from '../general/presentation/Carousel.vue'
+  import { mapGetters } from 'vuex'
+  import componentConfig from '../../data/componentConfig'
+
+  export default {
+    data () {
+      return {
+        picker: null,
+        backgroundSrc: '/static/images/slc-large-min-bright.jpg'
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'page'
+      ]),
+      backgroundStyle () {
+        return {
+          'background-image': 'url("' + this.backgroundSrc + '")',
+          'background-size': 'cover',
+          'background-position': 'center unset',
+          'background-repeat': 'no-repeat no-repeat'
+        }
+      }
+    },
+    components: {
+      [componentConfig.cards]: Cards,
+      [componentConfig.card]: Card,
+      [componentConfig.carousel]: Carousel
+    }
+  }
+</script>
