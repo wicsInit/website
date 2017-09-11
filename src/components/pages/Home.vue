076874af57f6ec1772adac1e510a3e7585c061a4
Cynicalbird <alex.ng.personal@gmail.com>
2017-09-11 00:25:57 -0400

    switched banner to small / cropped  version.

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -15,7 +15,7 @@
             <v-flex xs12 class="mb-3">
               <img
                 slot="media"
-                src="/static/images/team/DSC_0163.jpg"
+                src="/static/images/team/DSC_0163-small.jpg"
               >
             </v-flex>
             <!-- Home carousel -->
